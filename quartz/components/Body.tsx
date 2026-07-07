57f36660ca05a3b2e834017a10989f4bf3f55372
JiaLinLi0822 <ljl2282002@icloud.com>
2026-07-07 15:55:42 -0400

    Quartz sync: Jul 7, 2026, 3:55 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,18 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return (
+    <div id="quartz-body">
+      <a
+        class="global-home-button"
+        href="https://jialinli0822.github.io/"
+        aria-label="Back to Homepage"
+      >
+        ← Homepage
+      </a>
+      {children}
+    </div>
+  )
 }
 
 export default (() => Body) satisfies QuartzComponentConstructor
